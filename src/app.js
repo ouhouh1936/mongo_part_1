6818e4dbe96b8e1d624689f25879f96f5fd3f075
ouhouh1936 <ouhouh1936@gamil.com>
2020-10-19 22:53:42 +0900

    poijhgfdertgyhujk

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import connect from "../db/mongo"
+import Student from "../models/Student"
+
+dotenv.config();
+
+const app = express();
+
+connect();
+app.set(`PORT` , process.env.PORT);
+
+app.get("/" , async (req, res) => {
+    const result = await Student.find({}, {})
+
+    res.send(result);
+
+})
+
+
+app.listen(app.get(`PORT`), () => {
+    console.log(`SERVER START ::` , app.get(`PORT`));
+});ㄴ
